30a6c174a86c9493777242b1cc5087b07a6ae794
myu <myu@myujiku.com>
2022-12-06 18:34:10 +0100

    add doc comment

--- src/logic/user_data/mod.rs
+++ src/logic/user_data/mod.rs
@@ -15,4 +15,5 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+/// Module containing a serializable card collection data type.
 pub mod progressive_collection;
