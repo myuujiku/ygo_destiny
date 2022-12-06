4db1c79c769d8e6e0afeb633a046ed37d1c1239c
myu <myu@myujiku.com>
2022-12-06 09:21:16 +0100

    correct import in user_data module

--- src/logic/user_data/mod.rs
+++ src/logic/user_data/mod.rs
@@ -15,4 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-pub mod collection;
+pub mod progressive_collection;
