3cfcaa5b2022a2f3185ef27eee8d7b576c505163
myu <myu@myujiku.com>
2022-12-24 19:35:49 +0100

    export Progressive Collection

--- src/logic/user_data/mod.rs
+++ src/logic/user_data/mod.rs
@@ -17,3 +17,5 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 /// Module containing a serializable card collection data type.
 pub mod progressive_collection;
+
+pub use progressive_collection::ProgressiveCollection;
