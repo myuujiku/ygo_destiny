6401d2b3cf687086740da9a33eac57f0b11f8f92
myu <myu@myujiku.com>
2023-01-23 18:14:45 +0100

    add App to lib

--- src/lib.rs
+++ src/lib.rs
@@ -15,5 +15,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+mod app;
+
+pub use app::App;
+
 pub mod components;
 pub mod templates;
