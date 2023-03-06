d3de5f0909f60b74f03438006432fef8689b2cf5
myu <myu@myujiku.com>
2023-03-06 19:07:38 +0100

    update dependencies

--- build.rs
+++ build.rs
@@ -19,7 +19,7 @@ use glib_build_tools::compile_resources;
 
 fn main() {
     compile_resources(
-        "resources",
+        &["resources"],
         "resources/gresources.xml",
         "compiled.gresource",
     );
