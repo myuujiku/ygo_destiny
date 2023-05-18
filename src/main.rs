efa099ed2524ae9522e36d34c98b0762a9bb707c
myu <myujiku@proton.me>
2023-05-18 12:01:09 +0200

    ensure data directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::fs;
+
 use ygo_destiny;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
+
+    fs::create_dir_all(&*ygo_destiny::DATA_DIR)?;
     let db = rusqlite::Connection::open(&*ygo_destiny::DB_PATH).unwrap();
     ygo_destiny::db::update_and_log(&db);
+
+    Ok(())
 }
